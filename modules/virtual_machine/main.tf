3527f36643ce002800dde58d5aa791e60d4970fe
ANDSANTOS4546 <andre_nascimento4546@hotmail.com>
2026-06-01 16:26:09 -0300

    feat: initial terraform drift lab

--- modules/virtual_machine/main.tf
+++ modules/virtual_machine/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "this" {
 
   source_image_reference {
   publisher = "Canonical"
-  offer     = "ubuntu-24_04-lts"
+  offer     = "ubuntu-24_04-lts-gen2"
   sku       = "server"
   version   = "latest"
   }
